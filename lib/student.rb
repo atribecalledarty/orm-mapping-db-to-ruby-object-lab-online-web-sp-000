5750350babb3e93747839b377f8d559a2de14ece
Arthur Song <arth3rs0ng@gmail.com>
2019-04-21 04:14:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,8 +42,12 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 
+      WHERE grade = 9
     SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
